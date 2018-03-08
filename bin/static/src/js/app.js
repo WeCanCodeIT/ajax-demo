0011effc777ecd08277fd0244fed495da8db618b
Don Hamilton III <donhamiltoniii@gmail.com>
2018-03-07 21:43:09 -0500

    Update exmple and remove bin from being followed

--- bin/static/src/js/app.js
+++
@@ -1,6 +0,0 @@
-const $asideButton = $('#asideButton')
-const $asideContent = $('#asideContent')
-
-$asideButton.click(() => {
-	$asideContent.load('../sidebar.html')
-})
--- src/main/resources/static/src/js/app.js
+++ src/main/resources/static/src/js/app.js
@@ -1,6 +1,24 @@
-const $asideButton = $('#asideButton')
-const $asideContent = $('#asideContent')
+// With VanillaJS
 
-$asideButton.click(() => {
-	$asideContent.load('../sidebar.html')
-})
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("asideContent").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "../sidebar.html", true);
+xhttp.send();
+
+// With an interval for frequent updates
+
+//setInterval(function() {
+//	xhttp.open("GET", "../sidebar.html", true)
+//	xhttp.send()
+//}, 3000);
+
+// With jQuery
+
+//const $asideContent = $('#asideContent')
+//
+//$asideContent.load('../sidebar.html')
